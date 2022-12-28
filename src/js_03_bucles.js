c0ef7c56ab4e9223787f40f9fa4ba6286dd297dd
Marc <marc.a@bling.eu>
2022-12-28 17:42:45 +0100

    adding info

--- src/js_03_bucles.js
+++ src/js_03_bucles.js
@@ -1,21 +1,21 @@
 // bucle
 
 //while
-console.log("\n[While]")
+console.log("\n[while]")
 let i = 1
 while(i<4)
 {
-    console.log(`iteration: ${i}`);
+    console.log(`[while] iteration: ${i}`);
     i++
 }
 
 
 // do while (first iteration is always executed)
-console.log("\n[Do while]")
+console.log("\n[do while]")
 let j = 1
 do
 {
-    console.log(`iteration: ${j}`);
+    console.log(`[do while] iteration: ${j}`);
     j++
 } while(j<4)
 
@@ -23,5 +23,10 @@ do
 console.log("\n[For]")
 for (k = 1; k < 4; k++)
 {
-    console.log(`iteration: ${k}`);
+    console.log(`[while] iteration: ${k}`);
+    //stop bucle
+    if(k==3)
+    {
+        break
+    }
 }
